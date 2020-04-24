cd4202f3b7f0fe538f05ae364db2f6d0d44081b0
Douglas Fuelber <contato@douglasfuelber.com>
2020-04-23 22:42:15 -0300

    Added items to menu

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,3 @@
-const formatDate = (date: Date): string => {
-  return Intl.DateTimeFormat('pt-BR').format(new Date(date));
-};
-
+const formatDate = (date: Date): string =>
+  Intl.DateTimeFormat('pt-BR').format(new Date(date));
 export default formatDate;
